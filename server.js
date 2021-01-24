1e79cd4a06db9f04963e93e1e498d35e3441ae5f
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-01-24 14:38:33 -0700

    fixed auth

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.use(cors())
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
+  
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+  if (!authToken || authToken !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
   // move to the next middleware
